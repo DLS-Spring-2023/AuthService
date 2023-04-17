1ef330620464ccdbc2fdc91f44684ff86a92239a
Lars Riel <larsriel@gmail.com>
2023-04-17 13:24:56 +0200

    added copy functionality to project ID and API key

--- console/src/lib/components/Input.svelte
+++ console/src/lib/components/Input.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import FaEye from 'svelte-icons/fa/FaEye.svelte';
 	import FaEyeSlash from 'svelte-icons/fa/FaEyeSlash.svelte';
+	import Copy from 'svelte-icons/fa/FaRegCopy.svelte'
+	import Clipboard from 'clipboard';
 
 	export let type: 'text' | 'password' | 'email' = 'text';
 	export let name = '';
@@ -10,16 +12,49 @@
 	export let required = false;
 	export let disabled = false;
 	export let focus = false;
+	export let copy = false;
 	export let cursor: 'text' | 'pointer' | 'default' | undefined = undefined;
 
 	let hidePass = true;
 
-	const typeAction = (node: HTMLInputElement, hide: boolean) => {
+	const hidePassAction = (node: HTMLInputElement, hide: boolean) => {
 		node.type = type === 'password' ? (hide ? 'password' : 'text') : type;
 
 		return {
 			update(hide: boolean) {
 				node.type = type === 'password' ? (hide ? 'password' : 'text') : type;
+			}
+		};
+	};
+
+	let bg = 'bg-dark';
+	let duration = 'duration-100';
+	const copyToClipboard = (node: HTMLButtonElement) => {
+		const clipboard = new Clipboard(node, {
+			text: () => value
+		});
+
+		clipboard.on('success', () => {
+			bg = 'bg-green-600';
+			duration = 'duration-100';
+			setTimeout(() => {
+				bg = 'bg-dark';
+				duration = 'duration-300';
+			}, 300);
+		});
+
+		clipboard.on('error', () => {
+			bg = 'bg-red-500'
+			duration = 'duration-100'
+			setTimeout(() => {
+				bg = 'bg-dark';
+				duration = 'duration-300';
+			}, 1000);
+		});
+
+		return {
+			destroy() {
+				clipboard.destroy();
 			}
 		};
 	};
@@ -36,25 +71,32 @@
 		{placeholder}
 		{required}
 		{disabled}
-		use:typeAction={hidePass}
+		use:hidePassAction={hidePass}
 		use:focusInput={focus}
 		bind:value
 		class="
-            w-full bg-light dark:bg-dark p-2
+            w-full 
+			transition-colors ease-in-out {duration} {bg} p-2
             border border-solid border-slate-500 rounded-md
-            transition ease-linear duration-75 {cursor ? `hover:cursor-${cursor}` : ''}
+			{cursor ? `hover:cursor-${cursor}` : ''}
     "
 	/>
-	{#if type === 'password'}
+	{#if type === 'password' || copy}
 		<div
 			class="
-            absolute right-2 bottom-1 w-6 text-gray-400
-            hover:text-gray-50 hover:cursor-pointer
+            flex h-full items-center absolute right-2 top-0 text-gray-400 hover:cursor-pointer
         "
 		>
-			<button type="button" on:click|preventDefault={() => (hidePass = !hidePass)}>
-				{#if hidePass}<FaEye />{:else}<FaEyeSlash />{/if}
-			</button>
+			{#if copy}
+				<button type="button" use:copyToClipboard class="w-4 mr-2 hover:text-gray-50">
+					<Copy />
+				</button>
+			{/if}
+			{#if type === 'password'}
+				<button type="button" on:click|preventDefault={() => (hidePass = !hidePass)} class="w-6 hover:text-gray-50">
+					{#if hidePass}<FaEye />{:else}<FaEyeSlash />{/if}
+				</button>
+			{/if}
 		</div>
 	{/if}
 </div>
--- console/src/routes/(private)/console/project/[project_id]/+page.svelte
+++ console/src/routes/(private)/console/project/[project_id]/+page.svelte
@@ -9,9 +9,18 @@
 	<div
 		class="w-full bg-dark brightness-125 rounded-xl border-full p-4 flex justify-between items-center"
 	>
+		<label for="api_key" class="mr-4">Project ID</label>
+		<div class="hover:cursor-text w-80">
+			<Input type="text" id="api_key" disabled cursor="text" copy value={project.id} />
+		</div>
+	</div>
+
+	<div
+		class="w-full bg-dark brightness-125 rounded-xl border-full p-4 mt-12 flex justify-between items-center"
+	>
 		<label for="api_key" class="mr-4">API KEY</label>
-		<div class="hover:cursor-text w-72">
-			<Input type="password" id="api_key" disabled cursor="text" value={project.keystore.api_key} />
+		<div class="hover:cursor-text w-80">
+			<Input type="password" id="api_key" disabled cursor="text" copy value={project.keystore.api_key} />
 		</div>
 	</div>
 </div>
